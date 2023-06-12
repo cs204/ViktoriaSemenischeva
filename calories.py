a1b29784510a799a41ed8a4bec7276f619201200
test <test>
2023-06-12 19:40:31 +0700

    test

---
+++ calories.py
@@ -0,0 +1,17 @@
+
+fruits_calories = {
+    "Apple": 130,
+    "Banana": 110,
+    "Lime": 20,
+    "Avocado": 61,
+}
+
+
+fruit = input("Фрукт: ")
+
+
+if fruit in fruits_calories:
+    print(f"Калории: {fruits_calories[fruit]}")
+
+
+
